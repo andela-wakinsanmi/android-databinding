6a6ea08fb3c578f3137489cb3adf460755e2983a
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-04-03 17:55:25 +0100

    Use Realm in the MainActivity

--- app/src/main/java/com/andela/playdatabinding/MainActivity.java
+++ app/src/main/java/com/andela/playdatabinding/MainActivity.java
@@ -9,10 +9,15 @@ import android.util.Log;
 import com.andela.playdatabinding.adapter.DataRecyclerAdapter;
 import com.andela.playdatabinding.http.DataFetcher;
 import com.andela.playdatabinding.http.DataFetcherImpl;
+import com.andela.playdatabinding.model.Dog;
+import com.andela.playdatabinding.model.Person;
 import com.andela.playdatabinding.model.User;
 
 import java.util.List;
 
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -55,6 +60,46 @@ public class MainActivity extends AppCompatActivity {
                         dataRecyclerAdapter.swapList(users);
                     }
                 });
+
+        Dog dog = new Dog();
+        dog.setName("Rex");
+        dog.setAge(1);
+
+        Realm.init(this);
+        Realm realm = Realm.getDefaultInstance();
+        final RealmResults<Dog> puppies = realm.where(Dog.class).lessThan("age", 2).findAll();
+        puppies.size();
+        Log.d("waleola", "The size is = " + puppies.size());
+
+        puppies.addChangeListener(new RealmChangeListener<RealmResults<Dog>>() {
+            @Override
+            public void onChange(RealmResults<Dog> element) {
+                Log.d("waleola", "The vsize after adding is " + element.size());
+            }
+        });
+
+        realm.beginTransaction();
+
+        final Dog managedDog = realm.copyToRealm(dog);
+
+        Person person = realm.createObject(Person.class);
+        person.getDogs().add(managedDog);
+        realm.commitTransaction();
+
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Dog dog = realm.where(Dog.class).equalTo("age", 1).findFirst();
+                dog.setAge(3);
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                Log.d("waleola", "the new size of puppies " + puppies.size());
+                Log.d("waleola", "the managed dog's age is " + managedDog.getAge());
+            }
+        });
+
 
     }
 
